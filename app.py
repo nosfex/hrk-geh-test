c5eff8cbcb806a13ee3de4211a72a6e59a8e33d6
Gerardo Heidel <gerardo.heidel@nimblegiant.com>
2021-08-08 20:51:33 -0300

    database initialization

--- app.py
+++ app.py
@@ -1,8 +1,31 @@
 from flask import Flask, request, jsonify
+import psycopg2
+from config import config
 app = Flask(__name__)
 @app.route("/")
 def index():
     return "Hello World!"
+
+@app.route('/config_db/')
+def config_db():
+    conn = None
+    try:
+        params = config()
+        conn = psycopg2.connect(**params)
+
+        cur = conn.cursor()
+
+        cur.execute('SELECT version()')
+
+        db_version = cur.fecthone()
+        print(db_version)
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+            print('Connection closed')
 @app.route('/get/', methods=['GET'])
 def get_test():
     name = request.args.get("name",None)
@@ -26,7 +49,7 @@ def post_test():
     print(name)
 
     if name:
-        return jsonify({
+        return jsonify({               
             "Message": f"Welcome {name} to our awesome platform!!",
             # Add this option to distinct the POST request
             "METHOD" : "POST"
